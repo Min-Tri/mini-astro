672ef5eed3eab621e1b8cad34778ea995b9a5a10
Min-Tri <tmtri.290798@gmail.com>
2024-08-15 19:22:18 +0700

    [fix] fix to run

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { loadEnv } from "vite";
+const { STORYBLOK_TOKEN } = loadEnv(process.env.STORYBLOK_TOKEN, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     storyblok({
-      accessToken: import.meta.env.STORYBLOK_TOKEN,
+      accessToken: STORYBLOK_TOKEN,
       components: {
         page: 'storyblok/Page',
         feature: 'storyblok/Feature',
@@ -16,4 +19,10 @@ export default defineConfig({
       },
     }),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
